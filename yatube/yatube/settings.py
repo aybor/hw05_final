998972b6b958856193a8a7b2bdecb53edab567f6
a.borisov <borisov_alek@mail.ru>
2021-11-25 11:05:05 +0300

    Работоспособность восстановлена, статики нет

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'about.apps.AboutConfig',
     'sorl.thumbnail',
-    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -63,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
